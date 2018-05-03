799549b3b66b81f54577626f62dff17da9ca9777
Ravi Saxena <ravi.saxena234@gmail.com>
2018-05-03 14:33:24 +0530

    port

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
 
+app.get('/users',(request,response)=>{
+        response.json([{"user":'Asif'},{"user":'ayush'},{"user":"ravi"},{"user":"Abhimanyu"}]);
+})
+
+
 app.listen(process.env.PORT, () => {
     console.log("Server is listening on port 8081");
 });
